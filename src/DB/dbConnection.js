e957ad348db2e73356eb46724b82bfcc0f41437f
GayatriGurave <guravegayatri@gmail.com>
2025-11-28 15:22:24 +0530

    ok

--- src/DB/dbConnection.js
+++ src/DB/dbConnection.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
-let DB_URL = "mongodb://localhost:27017/demand_app_db"
+// let DB_URL = "mongodb://localhost:27017/demand_app_db"
+let DB_URL = "mongodb://mongo:27017/demand_app_db"
 
 async function connectionToDatabase(){
     try {
